216e04538f2d0f2b0aead195c9f0f6234105eaa9
Maykel Josue Menendez Vélez <josuemenendezv@gmail.com>
2026-07-19 11:38:05 -0500

    correccion de url

--- mobile/services/marketplaceApi.ts
+++ mobile/services/marketplaceApi.ts
@@ -59,7 +59,7 @@ export class ApiRequestError extends Error {
   }
 }
 
-export const apiBaseUrl = normalizeBaseUrl(process.env.EXPO_PUBLIC_API_BASE_URL ?? 'http://127.0.0.1:8000/api');
+export const apiBaseUrl = normalizeBaseUrl(process.env.EXPO_PUBLIC_API_BASE_URL ?? 'http://127.0.0.1:8010/api');
 
 function normalizeBaseUrl(value: string) {
   return value.replace(/\/+$/, '');
